ef2fcc2efb828e411f8096db74eaaa507bd3d68e
nat662434 <117282786+nat662434@users.noreply.github.com>
2024-01-29 07:53:27 +0100

    Actualizar 2-Evaluacion

---
+++ 2-Evaluacion/2Ariketa/prueba.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input v-model="newNumber" type="number" placeholder="Idatzi zenbaki bat" />
+    <button @click="addNumber" :disabled="isDisabled">Gehitu</button>
+    <ul>
+      <li v-for="(number, index) in numbers" :key="index">{{ number }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numbers: [],
+      newNumber: null,
+    };
+  },
+  computed: {
+    isDisabled() {
+      return this.newNumber === null || this.numbers.includes(this.newNumber);
+    },
+  },
+  methods: {
+    addNumber() {
+      if (this.newNumber !== null && !this.numbers.includes(this.newNumber)) {
+        this.numbers.push(this.newNumber);
+        this.newNumber = null;
+      }
+    },
+  },
+};
+</script>
